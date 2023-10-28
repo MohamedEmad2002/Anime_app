94f6e74ea4f5a08a5d884c292fc66179a2403e1f
MohamedEmad2002 <mohamedelbakrawy70@gmail.com>
2023-10-28 19:33:45 +0200

    "create cubit for register & add bloc observer"

---
+++ lib/bussiness_logic/cubits/register/cubit/register_state.dart
@@ -0,0 +1,13 @@
+part of 'register_cubit.dart';
+
+
+abstract class RegisterState {}
+
+class RegisterInitial extends RegisterState {}
+
+class RegisterLoadingState extends RegisterState {}
+class RegisterSuccessState extends RegisterState{}
+class RegisterErrorState extends RegisterState{
+  final String error;
+  RegisterErrorState(this.error);
+}
